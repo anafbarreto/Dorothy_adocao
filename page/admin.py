8addcca1f634225dcd3cc632bae75691bddc379a
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-03-27 23:32:27 -0300

    feat:implements config admin home

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# To do 
+# @admin.register(Home)
+# class HomeAdmin(admin.ModelAdmin):
+    
+#     list_display = [];
+#     search_fields = [];
+#     list_filter = [];
