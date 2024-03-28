68492b07db1caf5b63af9924ef841a060d6c0564
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-03-28 00:50:07 -0300

    feat: model animal and preview card front

--- page/models.py
+++ page/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    image_url = models.URLField()
