89d18ae730e42ebdda12cd99d28fa062f6f82f6f
Isadora Bagatini <isabag82@gmail.com>
2024-04-02 12:30:35 -0300

    primeiro commit do front da página inicial

--- projeto_final/settings.py
+++ projeto_final/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 # Adicione o caminho para o diretório onde seus arquivos estáticos estão localizados
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]
+ ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
