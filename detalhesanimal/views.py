3d171e6c4ae841e1eae010fec3aba977569fcd4c
ThaisAp10 <thaisapare.10@gmail.com>
2024-04-02 18:25:16 -0300

    Inserindo alterações da página detalhes do animal

--- detalhesanimal/views.py
+++ detalhesanimal/views.py
@@ -6,4 +6,5 @@ from banco.models import cadastro_animais
 def detalhes_animal(request, animal_id):
     animal = get_object_or_404(cadastro_animais, pk=animal_id)
     return render(request, 'detalhes_animal.html', {'animal': animal})
+    #return render(request, 'detalhes_animal.html')
 
--- projeto_final/urls.py
+++ projeto_final/urls.py
@@ -24,6 +24,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('detalhes_animal/', detalhes_animal, name='detalhes_animal')
+    path('detalhes_animal/<int:animal_id>/', detalhes_animal, name='detalhes_animal')
+]
 #    path('detalhes_animal/<int:animal_id>/', detalhes_animal, name='detalhes_animal'),  /* teste com uma imagem - - - apagar
 #    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
