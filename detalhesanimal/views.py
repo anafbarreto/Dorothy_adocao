92776a02662c99d9e61ae6a3bdb870009344698a
anafbarreto <karolbarreto10@gmail.com>
2024-04-02 18:44:21 -0300

    Ajuste commits detalhes animais

---
+++ detalhesanimal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+#from django.http import HttpResponse
+from banco.models import cadastro_animais
+
+# Create your views here.
+def detalhes_animal(request, animal_id):
+    animal = get_object_or_404(cadastro_animais, pk=animal_id)
+    return render(request, 'detalhes_animal.html', {'animal': animal})
+    #return render(request, 'detalhes_animal.html')
+
