6bd65476eb0309f9e2b9f1938934050ac0978a08
claricesoares <clarice.soares@ee.ufcg.edu.br>
2024-04-10 12:57:28 -0300

    fix: comentando codigo admin

--- banco/admin.py
+++ banco/admin.py
@@ -1,20 +1,20 @@
-from django.contrib import admin
-from banco.models import Adotante
-from banco.models import adocao
+# from django.contrib import admin
+# from banco.models import Adotante
+# from banco.models import adocao
 
 
-# Register your models here.
+# # Register your models here.
 
 
-@admin.register(Adotante)
-class CadastroAdmmin(admin.ModelAdmin):
-        list_display = ['nome', 'email', 'data']
-        search_fields = ['nome', 'email']
-        list_filter = ['data']
+# @admin.register(Adotante)
+# class CadastroAdmmin(admin.ModelAdmin):
+#         list_display = ['nome', 'email', 'data']
+#         search_fields = ['nome', 'email']
+#         list_filter = ['data']
 
 
-@admin.register(adocao)
-class CadastroAdmmin(admin.ModelAdmin):
-        list_display = ['nome', 'email', 'data']
-        search_fields = ['nome', 'email']
-        list_filter = ['data']
+# @admin.register(adocao)
+# class CadastroAdmmin(admin.ModelAdmin):
+#         list_display = ['nome', 'email', 'data']
+#         search_fields = ['nome', 'email']
+#         list_filter = ['data']
